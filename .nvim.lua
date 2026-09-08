95c245ba54c954cb385f5062bb3e789a38c2ce6f
eternasuno <eternasuno@users.noreply.github.com>
2026-09-08 01:48:31 +0000

    feat: add ts lsp

--- .nvim.lua
+++ .nvim.lua
@@ -1,4 +1,10 @@
+local root = vim.fs.root(0, { "tsconfig.json", "node_modules" }) or vim.fn.getcwd()
+
 vim.lsp.config("tsgo", {
+  cmd = { vim.fs.joinpath(root, "node_modules", ".bin", "tsc"), "--lsp", "--stdio" },
+  root_dir = function(_, on_dir)
+    on_dir(root)
+  end,
   on_attach = function(client)
     client.server_capabilities.documentFormattingProvider = false
     client.server_capabilities.documentRangeFormattingProvider = false
--- devenv.nix
+++ devenv.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}: {
+{...}: {
   languages = {
     javascript = {
       enable = true;
